b502218dfd557d8dc9ae959f842ba677c897993f
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2026-08-21 09:33:38 -0600

    Create fase5.asm for JIT engine implementation
    Add initial assembly file for JIT engine phase 5.

---
+++ JIT/fase5.asm
@@ -0,0 +1,4 @@
+; ============================================================================
+; JVMOS - JIT Engine (Fase 5: Parámetros Dinámicos y Convención de Llamada)
+; Formato: NASM x86 32-bit (Modo Protegido Bare-Metal)
+; ============================================================================
